83b5eeb022877559a19c50b0e13e011c38be8a9c
Burak Bolat <burakbolat_06@hotmail.com>
2023-03-19 23:21:05 +0300

    Added about me.

--- assets/js/main.js
+++ assets/js/main.js
@@ -146,9 +146,9 @@
     new Typed('.typed', {
       strings: typed_strings,
       loop: true,
-      typeSpeed: 100,
-      backSpeed: 50,
-      backDelay: 2000
+      typeSpeed: 75,
+      backSpeed: 25,
+      backDelay: 1000
     });
   }
 
